fb8261f986a9d9dc7efe9d984d18bd05cc42ba30
Blaysy <108405101+Blaysy@users.noreply.github.com>
2022-06-29 15:18:39 +0300

    Update Scrip.bat

--- Scrip.bat
+++ Scrip.bat
@@ -2,6 +2,6 @@ git.exe init -- Инициализация
 git.exe add . -- Добавление всех файлов (индексация файлов)
 git.exe commit -a -m "first commit" -- Коммит с комментом
 git.exe branch -M main -- Переключение ветки
-git.exe remote add origin https://github.com/Net2Fox/EKZ04.git -- Привязка репозитория с именем origin
+git.exe remote add origin https://github.com/Blaysy/FL004.git -- Привязка репозитория с именем origin
 git.exe push -u origin main -- Загрузка в репозиторий
 pause -- Остановка скрипта
